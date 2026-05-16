51a3c3f00b12abd4a8a59ad0333105c740024d71
Strol20 <131034301+Strol20@users.noreply.github.com>
2026-05-16 22:42:18 +0300

    audioFinder - path update

--- musicFinder/basic/shazamioFinder.py
+++ musicFinder/basic/shazamioFinder.py
@@ -1,12 +1,42 @@
 import asyncio
+
 from shazamio import Shazam
 
+from pathlib import Path
 
-async def main():
-  shazam = Shazam()
-  # out = await shazam.recognize_song('dora.ogg') # slow and deprecated, don't use this!
-  out = await shazam.recognize('dora.ogg')  # rust version, use this!
-  print(out)
 
-loop = asyncio.get_event_loop()
-loop.run_until_complete(main())
+
+async def musicFinder():
+
+    shazam = Shazam()
+
+
+
+    #file_path = r'D:\Platon\KPI_platon\SHAZAM_project\data\audioProces.wav'
+
+    root_dir = Path(__file__).resolve().parent.parent.parent
+    output_dir = root_dir / "data"
+    output_dir.mkdir(parents=True, exist_ok=True)
+    file_path = output_dir / "audioProces.wav"
+    print(f"Шукаємо музику за файлом: {file_path}")
+    try:
+
+        out = await shazam.recognize(str(file_path))
+
+        print("Результат распознавания:")
+
+        print(out)
+
+    except Exception as eror:
+
+        print(f"Помилка: {eror}")
+
+
+
+
+
+
+
+if __name__ == "__main__":
+
+    asyncio.run(musicFinder())
