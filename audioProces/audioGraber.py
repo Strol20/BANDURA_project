4225917282618b313048b9c4708546bdacf7b850
Strol20 <131034301+Strol20@users.noreply.github.com>
2026-05-16 17:48:45 +0300

    audioRecord - save audio to folder data and audioGraber

--- audioProces/audioGraber.py
+++ audioProces/audioGraber.py
@@ -1,5 +1,6 @@
 import soundcard as sc
 import soundfile as sf
+from pathlib import Path
 
 def getLoopbackMicrophone(speakerName = None):
     # print(sc.all_microphones(include_loopback=True))
@@ -14,6 +15,7 @@ def getLoopbackMicrophone(speakerName = None):
             print("Error:", error)
             speaker = sc.default_speaker()
 
+    # Пошук loopмікрофон динаміка
     loopSpeaker = sc.get_microphone(speaker.id,include_loopback=True )
     print(loopSpeaker)
     return loopSpeaker
@@ -27,19 +29,26 @@ def audioRecord(microphone = None, samplerate = 41000,duration=10):
     if (microphone == None):
         print("Не знайдено мікро, використовуємо за замовченням")
         return None
+    root_dir = Path(__file__).resolve().parent.parent
+    output_dir = root_dir / "data"
+    output_dir.mkdir(parents=True, exist_ok=True)
+    file_path = output_dir / "audioProces.wav"
     with microphone.recorder(samplerate=samplerate,channels=2) as recorder:
         print("Початок запису на...", duration, "секунд")
         data = recorder.record(numframes=samplerate * duration)
-        sf.write("filename.wav", data, samplerate)
-        print("КІНЕЦЬ ЗАПИСУ")
+        sf.write(str(file_path), data, samplerate)
+        print("КІНЕЦЬ ЗАПИСУ. Файл знаходиться в:", file_path)
+
+def audioGraber(speakerName):
+    audioRecord(getLoopbackMicrophone())
 
 
+#Для тесту
+#micro = sc.all_speakers()
 
-micro = sc.all_speakers()
+#print(micro)
 
-print(micro)
-
-audioRecord('aaa')
+#audioGraber('aaa')
 
 
 
