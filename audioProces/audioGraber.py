e2875b821a8102068f252d3526f52a3ad9d74be1
Strol20 <131034301+Strol20@users.noreply.github.com>
2026-05-16 15:59:38 +0300

    audioGraber - audioRecord

--- audioProces/audioGraber.py
+++ audioProces/audioGraber.py
@@ -1,8 +1,9 @@
 import soundcard as sc
 import soundfile as sf
 
-def audio_Graber(speakerName = None, samplerate = 44100):
+def getLoopbackMicrophone(speakerName = None):
     # print(sc.all_microphones(include_loopback=True))
+
     # Пошук мікро яке віртуально виводить звук з навушника (loopback)
     if (speakerName == None):
         speaker = sc.default_speaker()
@@ -12,15 +13,32 @@ def audio_Graber(speakerName = None, samplerate = 44100):
         except Exception as error:
             print("Error:", error)
             speaker = sc.default_speaker()
-    loopSpeaker = sc.get_microphone(speaker.name,include_loopback=True )
+
+    loopSpeaker = sc.get_microphone(speaker.id,include_loopback=True )
     print(loopSpeaker)
+    return loopSpeaker
+
+
+
+
+def audioRecord(microphone = None, samplerate = 48000,durationSec=10):
+    # Тривалість в секундах
+    microphone = getLoopbackMicrophone(microphone) #Перенести в audioGraber
+    if (microphone == None):
+        print("Не знайдено мікро, використовуємо за замовченням")
+        return None
+    with microphone.recorder(samplerate=samplerate,channels=2) as recorder:
+        data = recorder.record(numframes=samplerate * durationSec)
+        sf.write("filename.wav", data, samplerate)
+        print("КІНЕЦЬ ЗАПИСУ")
+
 
 
 micro = sc.all_speakers()
 
 print(micro)
 
-audio_Graber('aaa')
+audioRecord('aaa')
 
 
 
