2a2fe6b8bc1b2977017dec69254951d6ed0a19b0
Strol20 <131034301+Strol20@users.noreply.github.com>
2026-05-17 14:03:32 +0300

    fix shazamioFinder

--- audioProces/shazamioFinder.py
+++ audioProces/shazamioFinder.py
@@ -7,9 +7,9 @@ from pathlib import Path
 
 async def shazamio_finder():
     shazam = Shazam()
-    #Тестами було помічено, що апі аналізує не повністю аудіо файл. Зазвичай це не дуже популярні треки
+    #Тестами було помічено, що апі не може знайти деякі аудіофайли, які можу знайти шазма. Зазвичай це не дуже популярні треки
     #file_path = r'D:\Platon\KPI_platon\SHAZAM_project\data\audioProces.wav'
-    root_dir = Path(__file__).resolve().parent.parent.parent
+    root_dir = Path(__file__).resolve().parent.parent
     output_dir = root_dir / "data"
     output_dir.mkdir(parents=True, exist_ok=True)
     file_path = output_dir / "audioProces.wav"
